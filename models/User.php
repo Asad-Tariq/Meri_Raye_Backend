f865c689323d55e14b2625581411f66e8fb5b1ec
Asad-Tariq <at05439@st.habib.edu.pk>
2022-05-16 15:51:45 +0500

    Added models and APIs for User and Reviews
    Co-Authored-By: Fahad Shaikh <60060979+shaikh-fahad@users.noreply.github.com>

--- models/User.php
+++ models/User.php
@@ -3,48 +3,68 @@
 $ds = DIRECTORY_SEPARATOR;
 $base_dir = realpath(dirname(__FILE__). $ds . '..') . $ds;
 
-require_once("{$base_dir}includes{$ds}Database.php"); // including the DB
+require_once("{$base_dir}includes{$ds}Database.php"); // Including database
+require_once("{$base_dir}includes{$ds}Bcrypt.php"); // Including Bcrypt
 
-class User {
-    private $table = 'user';
+// Class User Start
+class User
+{
+    private $table = 'users';
 
     public $id;
-    public $user_name;
+    public $name;
+    public $email;
     public $password;
-    public $rating;
-    public $following;
-    public $followers;
-    public $bio;
     public $image;
+    public $address;
+    public $description;
 
-    // constructor
-    public function __construct() {}
+    // contructor
+    public function __construct()
+    {
+    }
 
-    // function to validate if params exist or not
-    public function validate_params($value) {
+    // validating if params exists or not
+    public function validate_params($value)
+    {
+        // print "value = " .$value;
         return (!empty($value));
     }
 
-    // function to save new data in database
-    public function register_user() {
+    // to check if email is unique or not
+    public function check_unique_email()
+    {
         global $database;
 
-        $this->user_name = trim(htmlspecialchars(strip_tags($this->user_name)));
-        $this->password = trim(htmlspecialchars(strip_tags($this->password)));
-        $this->rating = trim(htmlspecialchars(strip_tags($this->rating)));
-        $this->following = trim(htmlspecialchars(strip_tags($this->following)));
-        $this->followers = trim(htmlspecialchars(strip_tags($this->followers)));
-        $this->bio = trim(htmlspecialchars(strip_tags($this->bio)));
-        $this->image = trim(htmlspecialchars(strip_tags($this->image)));
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
 
-        $sql = "INSERT INTO $this->table (user_name, password, rating, following, followers, bio, image) VALUES (
-            '" .$database->escape_value($this->user_name). "',
-            '" .$database->escape_value($this->password). "',
-            '" .$database->escape_value($this->rating). "',
-            '" .$database->escape_value($this->following). "',
-            '" .$database->escape_value($this->followers). "',
-            '" .$database->escape_value($this->bio). "',
-            '" .$database->escape_value($this->image). "'
+        $sql = "SELECT id FROM $this->table WHERE email = '" .$database->escape_value($this->email). "'";
+
+        $result = $database->query($sql);
+        $user_id = $database->fetch_row($result);
+
+        return empty($user_id);
+    }
+
+    // saving new data in our database
+    public function register_user()
+    {
+        global $database;
+
+        $this->name = trim(htmlspecialchars(strip_tags($this->name)));
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+        $this->password = trim(htmlspecialchars(strip_tags($this->password)));
+        $this->image = trim(htmlspecialchars(strip_tags($this->image)));
+        $this->address = trim(htmlspecialchars(strip_tags($this->address)));
+        $this->description = trim(htmlspecialchars(strip_tags($this->description)));
+
+        $sql = "INSERT INTO $this->table (name, email, password, image, address, description) VALUES (
+            '" .$database->escape_value($this->name). "',
+            '" .$database->escape_value($this->email). "',
+            '" .$database->escape_value(Bcrypt::hashPassword($this->password)). "',
+            '" .$database->escape_value($this->image). "',
+            '" .$database->escape_value($this->address). "',
+            '" .$database->escape_value($this->description). "'
         )";
 
         $user_saved = $database->query($sql);
@@ -55,7 +75,43 @@ class User {
             return false;
         }
     }
-}
+
+    // login function
+    public function login()
+    {
+        global $database;
+
+        $this->email = trim(htmlspecialchars(strip_tags($this->email)));
+        $this->password = trim(htmlspecialchars(strip_tags($this->password)));
+
+        $sql = "SELECT * FROM $this->table WHERE email = '" .$database->escape_value($this->email). "'";
+
+        $result = $database->query($sql);
+        $user = $database->fetch_row($result);
+
+        if (empty($user)) {
+            return "Incorrect credentials.";
+        } else {
+            if (Bcrypt::checkPassword($this->password, $user['password'])) {
+                unset($user['password']);
+                return $user;
+            } else {
+                return "Incorrect credentials.";
+            }
+        }
+    }
+
+    // method to return the list of users
+    public function all_users() {
+        global $database;
+
+        $sql = "SELECT id, name, image, address FROM $this->table";
+
+        $result = $database->query($sql);
+
+        return $database->fetch_array($result);
+    }
+} // Class Ends
 
 // User object
 $user = new User();
